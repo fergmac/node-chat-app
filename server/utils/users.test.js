4d34481aeaf1b79e4852daf08ebbf8ffdbfd65a9
fergmac <f.macconnell@gmail.com>
2017-11-29 17:10:36 -0800

    fix should add new user test and add updated screenshot

--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -2,7 +2,7 @@ const expect = require('expect');
 const { Users } = require('./users');
 
 describe('Users', () => {
-    const users;
+    let users;
 
     beforeEach(() => {
         users = new Users();
@@ -15,63 +15,64 @@ describe('Users', () => {
             id: '2',
             name: 'Mike',
             room: 'React Course'
-        },{
+        },
+        {
             id: '3',
             name: 'Luke',
             room: 'Node Course'
-        }]
+        }];
     });
 
     it('should add new user', () => {
-        var users = new Users();
-        var user = {
+        users = new Users();
+        const user = {
             id: '123',
             name: 'Ferg',
             room: 'Music fans'
         };
-        var responseUser = users.addUser(user.id, user.name, user.room);
+        const resUser = users.addUser(user.id, user.name, user.room);
 
-        expect(users.users).toEqual([user]);
+        expect(users.users).toEqual([resUser]);
     });
 
     it('should remove a user', () => {
-        var userId = '1';
-        var user = users.removeUser(userId);
+        const userId = '1';
+        const user = users.removeUser(userId);
 
         expect(user.id).toBe(userId);
         expect(users.users.length).toBe(2);
     });
 
     it('should not remove user', () => {
-        var userId = '99';
-        var user = users.removeUser(userId);
+        const userId = '99';
+        const user = users.removeUser(userId);
 
         expect(user).toNotExist();
         expect(users.users.length).toBe(3);
     });
 
     it('should find user', () => {
-        var userId = '2';
-        var user = users.getUser(userId);
+        const userId = '2';
+        const user = users.getUser(userId);
 
         expect(user.id).toBe(userId);
     });
 
     it('should not find user', () => {
-        var userId = '99';
-        var user = users.getUser(userId);
+        const userId = '99';
+        const user = users.getUser(userId);
 
         expect(user).toNotExist();
     });
 
     it('should return names for node course', () => {
-        var userList = users.getUserList('Node Course');
+        const userList = users.getUserList('Node Course');
 
         expect(userList).toEqual(['Ferg', 'Luke']);
     });
 
     it('should return names for react course', () => {
-        var userList = users.getUserList('React Course');
+        const userList = users.getUserList('React Course');
 
         expect(userList).toEqual(['Mike']);
     });
